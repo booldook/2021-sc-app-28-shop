d93d436baf20946d54a64fc120c963357aadec0f
booldook <booldook@gmail.com>
2021-11-01 09:54:51 +0900

    /public/js

--- routes/admin/user-router.js
+++ routes/admin/user-router.js
@@ -1,27 +1,38 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
-const { error, telNumber } = require('../../modules/util')
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const { error, telNumber } = require('../../modules/util');
 
 // 회원리스트
 router.get('/', (req, res, next) => {
-  res.render('admin/user/user-list', { css: 'admin-user' })
-})
+  const ejs = {
+    telNumber,
+    type: req.query.type || '',
+  };
+  if (ejs.type === 'create') {
+    res.render('admin/user/user-form', ejs);
+  } else {
+    res.render('admin/user/user-list', ejs);
+  }
+});
 
 // 회원 수정 화면
 router.get('/:id', (req, res, next) => {
-  // type 분기
-  res.render('admin/user/user-form', { css: 'admin-user', telNumber })
-})
+  const ejs = {
+    telNumber,
+    type: req.query.type || '',
+  };
+  res.render('admin/user/user-form', ejs);
+});
 
 // 회원 수정
 router.put('/', (req, res, next) => {
-  res.send('/admin/user:PUT')
-})
+  res.send('/admin/user:PUT');
+});
 
 // 회원 삭제
 router.delete('/', (req, res, next) => {
-  res.send('/admin/user:DELETE')
-})
+  res.send('/admin/user:DELETE');
+});
 
-module.exports = { name: '/user', router }
+module.exports = { name: '/user', router };
