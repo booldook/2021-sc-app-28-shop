6e920950146924b98fe6182dbf64b3cc38e4181a
booldook <booldook@gmail.com>
2021-11-01 10:25:23 +0900

    우편번호 완료

--- public/js/user/user.js
+++ public/js/user/user.js
@@ -10,6 +10,23 @@ function openPostcode() {
       f.addrPost.value = post;
       f.addrRoad.value = road;
       f.addrJibun.value = jibun;
+
+      var extraRoadAddr = '';
+      if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+        extraRoadAddr += data.bname;
+      }
+      if (data.buildingName !== '' && data.apartment === 'Y') {
+        extraRoadAddr +=
+          extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName;
+      }
+      if (extraRoadAddr !== '') {
+        extraRoadAddr = ' (' + extraRoadAddr + ')';
+      }
+      if (data.address !== '') {
+        f.addrComment.value = extraRoadAddr;
+      } else {
+        f.addrComment.value = '';
+      }
     },
   }).open();
 }
