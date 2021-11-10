6e7c85000a1820e72624f5fb45a12ee035300629
booldook <booldook@gmail.com>
2021-11-10 12:08:41 +0900

    cate.js

--- public/js/cate/cate.js
+++ public/js/cate/cate.js
@@ -1,10 +1,12 @@
-/* var core = {};
-var plugins = ['contextmenu', 'dnd', 'state', 'wholerow', 'changed'];
+var core = {};
+var plugins = ['contextmenu', 'dnd', 'search', 'state', 'wholerow', 'changed'];
 
 core.themes = {
   variant: true,
   striped: true,
 };
+
+core.check_callback = true;
 
 core.data = {
   url: function (node) {
@@ -23,42 +25,3 @@ function onChangedTree(e, data) {
 $('#jstreeWrap')
   .jstree({ core: core, plugins: plugins })
   .on('changed.jstree', onChangedTree);
- */
-
-$('#jstreeWrap').jstree({
-  core: {
-    animation: 0,
-    check_callback: true,
-    themes: {
-      stripes: true,
-    },
-    data: {
-      url: function (node) {
-        return node.id === '#' ? '/ajax_demo_roots.json' : '/ajax_demo_children.json';
-      },
-      data: function (node) {
-        return {
-          id: node.id,
-        };
-      },
-    },
-  },
-  types: {
-    '#': {
-      max_children: 1,
-      max_depth: 4,
-      valid_children: ['root'],
-    },
-    root: {
-      valid_children: ['default'],
-    },
-    default: {
-      valid_children: ['default', 'file'],
-    },
-    file: {
-      icon: 'glyphicon glyphicon-file',
-      valid_children: [],
-    },
-  },
-  plugins: ['contextmenu', 'dnd', 'search', 'state', 'types', 'wholerow'],
-});
