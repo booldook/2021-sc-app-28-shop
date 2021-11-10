dd8000d7963286b6672efa2a9c77a78a6ae41756
booldook <booldook@gmail.com>
2021-11-10 13:06:37 +0900

    tree json 완료

--- public/js/cate/cate.js
+++ public/js/cate/cate.js
@@ -19,7 +19,6 @@ core.data = {
 
 function onChangedTree(e, data) {
   var json = $('#jstreeWrap').jstree(true).get_json('#');
-  console.log(json);
   axios
     .post('/api/tree', { json })
     .then(function (r) {
@@ -30,6 +29,22 @@ function onChangedTree(e, data) {
     });
 }
 
+function onUpdateTree() {
+  var json = $('#jstreeWrap').jstree(true).get_json('#');
+  axios
+    .post('/api/tree', { json })
+    .then(function (r) {
+      console.log('hi');
+      $('#jstreeWrap').jstree().refresh();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
 $('#jstreeWrap')
   .jstree({ core: core, plugins: plugins })
-  .on('changed.jstree', onChangedTree);
+  // .on('changed.jstree', onChangedTree)
+  .on('rename_node.jstree', onUpdateTree)
+  .on('move_node.jstree', onUpdateTree)
+  .on('delete_node.jstree', onUpdateTree);
