a5cba72bad776125fd6ae33c2718c2b0351df115
booldook <booldook@gmail.com>
2021-11-10 12:04:45 +0900

    cate.json

--- public/js/cate/cate.js
+++ public/js/cate/cate.js
@@ -1,4 +1,4 @@
-var core = {};
+/* var core = {};
 var plugins = ['contextmenu', 'dnd', 'state', 'wholerow', 'changed'];
 
 core.themes = {
@@ -23,3 +23,42 @@ function onChangedTree(e, data) {
 $('#jstreeWrap')
   .jstree({ core: core, plugins: plugins })
   .on('changed.jstree', onChangedTree);
+ */
+
+$('#jstreeWrap').jstree({
+  core: {
+    animation: 0,
+    check_callback: true,
+    themes: {
+      stripes: true,
+    },
+    data: {
+      url: function (node) {
+        return node.id === '#' ? '/ajax_demo_roots.json' : '/ajax_demo_children.json';
+      },
+      data: function (node) {
+        return {
+          id: node.id,
+        };
+      },
+    },
+  },
+  types: {
+    '#': {
+      max_children: 1,
+      max_depth: 4,
+      valid_children: ['root'],
+    },
+    root: {
+      valid_children: ['default'],
+    },
+    default: {
+      valid_children: ['default', 'file'],
+    },
+    file: {
+      icon: 'glyphicon glyphicon-file',
+      valid_children: [],
+    },
+  },
+  plugins: ['contextmenu', 'dnd', 'search', 'state', 'types', 'wholerow'],
+});
