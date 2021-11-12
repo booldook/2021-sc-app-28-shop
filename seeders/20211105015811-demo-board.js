e0c9b2c6c4a5edd5b55adda73789c009d731e32f
booldook <booldook@gmail.com>
2021-11-12 10:16:42 +0900

    seeder 수정

--- seeders/20211105015811-demo-board.js
+++ seeders/20211105015811-demo-board.js
@@ -2,41 +2,35 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const insertBoard = [];
-    const insertFile = [];
+    const insertNotice = [];
+    const insertGallery = [];
     for (let i = 1; i <= 120; i++) {
-      insertBoard.push({
-        binit_id: i < 50 ? 1 : 2,
-        user_id: i < 10 ? 1 : i,
-        title: '데모 입니다' + i,
+      insertNotice.push({
+        binit_id: '1',
+        user_id: 1,
+        title: '공지사항 입니다. ' + i,
+        writer: '최고관리자',
+        content: '공지사항 내용 입니다. ' + i,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+    for (let i = 1; i <= 120; i++) {
+      insertGallery.push({
+        binit_id: 2,
+        user_id: (i % 100) + 1,
+        title: '갤러리 데모 입니다. ' + i,
         writer: '데모 유저' + i,
-        content: '데모 내용 입니다.' + i,
+        content: '데모 내용 입니다. ' + i,
         createdAt: new Date(),
         updatedAt: new Date(),
       });
     }
-    await queryInterface.bulkInsert('Board', insertBoard);
-    const files = [
-      '211105_f07a2533-60e7-4c53-8479-2c189576ed2c.jpg',
-      '211104_65fde3ff-52df-4195-af72-ef588708a797.jpg',
-      '211104_c094c39a-dd2d-4f9d-8639-4c52b7d3346b.jpg',
-    ];
-    for (let i = 50; i < 120; i++) {
-      insertFile.push({
-        board_id: i,
-        oriName: '파일' + i + '.jpg',
-        saveName: files[Math.floor(Math.random() * 3)],
-        mimeType: 'image/jpg',
-        fileType: 'I',
-        size: 12369,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      });
-    }
-    await queryInterface.bulkInsert('BoardFile', insertFile);
+    await queryInterface.bulkInsert('board', insertNotice);
+    await queryInterface.bulkInsert('board', insertGallery);
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Board', null, {});
+    await queryInterface.bulkDelete('board', null, {});
   },
 };
