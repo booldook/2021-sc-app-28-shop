d4b78266c19425c9887b4143c625fd9cc2e75e45
booldook <booldook@gmail.com>
2021-11-12 10:55:00 +0900

    Seed Cate 까지 완성

--- seeders/20211105016000-demo-cate.js
+++ seeders/20211105016000-demo-cate.js
@@ -4,7 +4,7 @@ const { findChildId } = require('../modules/util');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const insertCate = [];
-    const jsonFile = fs.readJsonSync(path.join(__dirname, '../json/tree.json'));
+    const [jsonFile] = fs.readJsonSync(path.join(__dirname, '../json/tree.json'));
     const cateIds = findChildId(jsonFile, []);
     for (let v of cateIds) {
       insertCate.push({
