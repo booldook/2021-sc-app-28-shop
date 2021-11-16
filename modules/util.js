2d3ef30f7cd9da8009636447a9c1f013082d80c4
booldook <booldook@gmail.com>
2021-11-16 15:01:06 +0900

    03.sharp 완료

--- modules/util.js
+++ modules/util.js
@@ -52,20 +52,25 @@ const zipExt = ['zip', 'alz'];
 const exts = { imgExt, mediaExt, docExt, zipExt };
 
 const relPath = (file) => `/uploads/${file.split('_')[0]}/${file}`;
+
+const relThumbPath = (file) => `/uploads/${file.split('_')[0]}/thumb/${file}`;
+
 const absPath = (file) =>
   path.join(__dirname, `../storages/${file.split('_')[0]}/${file}`);
+
 const absThumbPath = (file) =>
   path.join(__dirname, `../storages/${file.split('_')[0]}/thumb/${file}`);
+
 const moveFile = async (file) => {
   try {
     let savePath = path.join(__dirname, '../storages-remove', file.split('_')[0]);
+    let saveThumbPath = path.join(savePath, 'thumb');
     let oldPath = absPath(file);
     await fs.ensureDir(savePath); // D:\ ~ /210909
     savePath = path.join(savePath, file); // D:\ ~ /210909/210909_fjk2134-askdf2103.jpg
     await fs.move(oldPath, savePath);
 
     if (await fs.pathExists(absThumbPath(file))) {
-      let saveThumbPath = path.join(savePath, 'thumb');
       let oldThumbPath = absThumbPath(file);
       await fs.ensureDir(saveThumbPath);
       saveThumbPath = path.join(saveThumbPath, file);
@@ -187,6 +192,7 @@ module.exports = {
   chgStatus,
   exts,
   relPath,
+  relThumbPath,
   absPath,
   absThumbPath,
   getIcon,
