e7d6d153d797975d1939aed1ce73bf85cbc060ea
booldook <booldook@gmail.com>
2021-11-16 13:28:55 +0900

    sharp 적용 중

--- modules/util.js
+++ modules/util.js
@@ -57,11 +57,7 @@ const absPath = (file) =>
 const moveFile = async (file) => {
   try {
     const isExist = await fs.pathExists(
-      __dirname,
-      '../storages/',
-      file.substr(0, 6),
-      './thumb',
-      file
+      path.join(__dirname, '../storages/', file.substr(0, 6), './thumb', file)
     );
     let savePath = path.join(__dirname, '../storages-remove', file.split('_')[0]);
     let oldPath = absPath(file);
