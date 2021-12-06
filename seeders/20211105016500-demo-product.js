c374a62287360d5872f8f91c70de8c25a8cd8c5c
booldook <booldook@gmail.com>
2021-12-06 12:05:06 +0900

    ...

--- seeders/20211105016500-demo-product.js
+++ seeders/20211105016500-demo-product.js
@@ -5,18 +5,18 @@ module.exports = {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
     );
-    for (let i = 0; i < 1000; i++) {
-      if (i === 100) i = 0;
-      if (i % 3 === 0) {
-        posts[i].body = '&lt;h2&gt;' + posts[i].body + '&lt;/h2&gt;';
+    for (let i = 0, j = 0; i < 1000; i++, j++) {
+      if (j === 100) j = 0;
+      if (j % 3 === 0) {
+        posts[j].body = '&lt;h2&gt;' + posts[j].body + '&lt;/h2&gt;';
       }
       products.push({
-        title: posts[i].title.split(' ')[0] + ' ' + posts[i].title.split(' ')[1],
+        title: posts[j].title.split(' ')[0] + ' ' + posts[j].title.split(' ')[1],
         priceOrigin: Math.floor(Math.random() * 300000) + 30000,
         priceSale: Math.floor(Math.random() * 300000) + 30000,
-        summary: posts[i].title,
-        content: posts[i].body,
-        amount: i % 20 === 0 ? 0 : Math.floor(Math.random() * 1000),
+        summary: posts[j].title,
+        content: posts[j].body,
+        amount: j % 20 === 0 ? 0 : Math.floor(Math.random() * 1000),
         createdAt: new Date(),
         updatedAt: new Date(),
       });
