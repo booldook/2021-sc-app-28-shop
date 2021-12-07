9f39ccd28324bc9816a7b22ad1fa7a063709ec79
booldook <booldook@gmail.com>
2021-12-07 11:09:12 +0900

    color-list

---
+++ public/js/color/color.js
@@ -0,0 +1,16 @@
+function onColorChange(el) {
+  changeColor($(el).val(), el);
+}
+
+function onColorReset(el) {
+  var color = $(el.form.defaultColor).val();
+  changeColor(color, el);
+  $(el.form.code).val(color);
+  $(el.form.name).val('');
+}
+
+function changeColor(color, el) {
+  $(el.form).find('.color-wrap .circle').css('background-color', color);
+  $(el.form).find('.color-wrap .hexa-code').css('color', color);
+  $(el.form).find('.color-wrap .hexa-code').text(color);
+}
